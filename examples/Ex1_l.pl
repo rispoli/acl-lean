7179ee2324230fbb4998fcec2f13b47c4bbddb88
Daniele Rispoli <daniele.rispoli@gmail.com>
2011-02-16 19:56:44 +0100

    deleted:    UNLICENSE 	new file:   COPYING 	modified:   deduction_tree.pl 	modified:   depth_distance.pl 	modified:   examples/CondACL_axioms.pl 	modified:   examples/CondACL_axioms_l.pl 	modified:   examples/Ex1.pl 	modified:   examples/Ex1_l.pl 	modified:   examples/Ex2.pl 	modified:   examples/Ex2_l.pl 	modified:   examples/Ex3.pl 	modified:   examples/Ex3_l.pl 	modified:   examples/tests.pl 	modified:   examples/tests_l.pl 	modified:   gnu_prolog/deduction_tree_c.c 	modified:   gnu_prolog/to_c.pl 	modified:   gnu_prolog/utilities_gnu.pl 	modified:   inference_rules_axioms.pl 	modified:   latexify.pl 		s/Public Domain License/GPLv3/

--- examples/Ex1_l.pl
+++ examples/Ex1_l.pl
@@ -1,3 +1,24 @@
+/*
+
+   Copyright 2010 Daniele Rispoli, Valerio Genovese
+
+   This file is part of ACL-Lean.
+
+   ACL-Lean is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   ACL-Lean is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with ACL-Lean. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 % Ex1.pl
 % ad says df1 -> df1: administrator has direct permission over delete file 1
 % ad says (b says df1 -> df1): administrator trusts bob on delete file 1
--- examples/Ex2_l.pl
+++ examples/Ex2_l.pl
@@ -1,3 +1,24 @@
+/*
+
+   Copyright 2010 Daniele Rispoli, Valerio Genovese
+
+   This file is part of ACL-Lean.
+
+   ACL-Lean is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   ACL-Lean is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with ACL-Lean. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 % Ex2.pl
 % (a_1 says (suu_1 -> wpu_1)): administrator 1 says that if user 1 is super user then
 %                              she has write permissions
